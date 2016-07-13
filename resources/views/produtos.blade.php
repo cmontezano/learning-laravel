dd98975a6850d0e22e05a1aafb14ddedc5ab91db
Carlos Montezano <cmontezanol@gmail.com>
2016-07-12 23:54:27 -0300

    make produtos extends from app default blade template

--- resources/views/produtos.blade.php
+++ resources/views/produtos.blade.php
@@ -1,14 +1,12 @@
-<html>
-    <head>
-        <title>Produtos</title>
-    </head>
-    <body>
-        <h1>Produtos</h1>
+@extends('app')
 
-        <ul>
-            @foreach($produtos as $produto)
-                <li>{{ $produto->nome }} ({{ $produto->descricao }})</li>
-            @endforeach
-        </ul>
-    </body>
-</html>
+@section('content')
+<div class="container">
+    <h1>Produtos</h1>
+    <ul>
+        @foreach($produtos as $produto)
+            <li>{{ $produto->nome }} ({{ $produto->descricao }})</li>
+        @endforeach
+    </ul>
+</div>
+@endsection
