5edf771cef4ad03e24284ba536de4f697c48deb9
Carlos Montezano <cmontezanol@gmail.com>
2016-07-13 00:11:12 -0300

    create table to show products list

--- resources/views/produtos.blade.php
+++ resources/views/produtos.blade.php
@@ -3,10 +3,26 @@
 @section('content')
 <div class="container">
     <h1>Produtos</h1>
-    <ul>
+    
+    <table class="table table-striped table-bordered table-hover">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nome</th>
+                <th>Descrição</th>
+                <th>Ação</th>
+            </tr>
+        </thead>
+        <tbody>
         @foreach($produtos as $produto)
-            <li>{{ $produto->nome }} ({{ $produto->descricao }})</li>
+            <tr>
+                <td>{{ $produto->id }}</td>
+                <td>{{ $produto->nome }}</td>
+                <td>{{ $produto->descricao }}</td>
+                <td></td>
+            </tr>
         @endforeach
-    </ul>
+        </tbody>
+    </table>
 </div>
 @endsection
