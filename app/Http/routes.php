bb71bb022888038b6beeaa799e1ca61c2b72da2f
Carlos Montezano <cmontezanol@gmail.com>
2016-07-12 23:10:59 -0300

    create get produtos route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('produtos', 'ProdutosController@index');
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
