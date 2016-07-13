026a40adb53c89c4217aa7cb57e30b955a5fa728
Carlos Montezano <cmontezanol@gmail.com>
2016-07-12 23:40:52 -0300

    using blade to list all products

--- resources/views/produtos.blade.php
+++ resources/views/produtos.blade.php
@@ -3,6 +3,12 @@
         <title>Produtos</title>
     </head>
     <body>
-        <h1>Olá {{ $nome }}</h1>
+        <h1>Produtos</h1>
+
+        <ul>
+            @foreach($produtos as $produto)
+                <li>{{ $produto->nome }} ({{ $produto->descricao }})</li>
+            @endforeach
+        </ul>
     </body>
 </html>
