0bff185b660c374b614c3a3b6e01f952b67fbe22
Carlos Montezano <cmontezanol@gmail.com>
2016-07-13 19:23:15 -0300

    fix method

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -6,5 +6,5 @@ Route::group(['middleware' => 'web'], function () {
     Route::get('produtos', 'ProdutosController@index');
     Route::get('produtos/create', 'ProdutosController@create');
     Route::post('produtos/store', 'ProdutosController@store');
-    Route::post('produtos/{id}/destroy', 'ProdutosController@destroy');
+    Route::get('produtos/{id}/destroy', 'ProdutosController@destroy');
 });
