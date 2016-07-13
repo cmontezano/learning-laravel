e27aea2217b87bd4194dc8afb7df06797e457b27
Carlos Montezano <cmontezanol@gmail.com>
2016-07-13 00:21:01 -0300

    fix assets including

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Laravel</title>
         
-        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
         <!-- Fonts -->
         <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media quer\
         ies -->
@@ -51,7 +51,7 @@
         </nav>
         @yield('content')
         <!-- Scripts -->
-        <script src="js/jquery-2.2.4.min.js"></script>
-        <script src="js/bootstrap.min.js"></script>
+        <script src="/js/jquery-2.2.4.min.js"></script>
+        <script src="/js/bootstrap.min.js"></script>
     </body>
 </html>
