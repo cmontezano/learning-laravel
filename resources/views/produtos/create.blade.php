24be766dc475abc7940bbe4ad591b4e39a896c95
Carlos Montezano <cmontezanol@gmail.com>
2016-07-13 01:19:03 -0300

    create form

--- resources/views/produtos/create.blade.php
+++ resources/views/produtos/create.blade.php
@@ -3,5 +3,23 @@
 @section('content')
     <div class="container">
         <h1>Novo Produto</h1>
+
+        {!! Form::open() !!}
+
+        <div class="form-group">
+            {!! Form::label('nome', 'Nome:') !!}
+            {!! Form::text('nome', null, ['class'=>'form-control']) !!}
+        </div>
+
+        <div class="form-group">
+            {!! Form::label('descricao', 'Descrição:') !!}
+            {!! Form::textarea('descricao', null, ['class'=>'form-control']) !!}
+        </div>
+
+        <div class="form-group">
+            {!! Form::submit('Criar Porduto', ['class'=>'btn btn-primary']) !!}
+        </div>
+
+        {!! Form::close() !!}
     </div>
 @endsection
