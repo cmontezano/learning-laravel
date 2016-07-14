1d286297b4ac1c96de996499195b5e0fdabf4183
Carlos Montezano <cmontezanol@gmail.com>
2016-07-13 22:48:53 -0300

    create names for each route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,9 +1,14 @@
 <?php
 Route::group(array('middleware' => 'web', 'prefix' => 'produtos'), function () {
-    Route::get('', 'ProdutosController@index');
-    Route::get('create', 'ProdutosController@create');
-    Route::post('store', 'ProdutosController@store');
-    Route::get('{id}/destroy', 'ProdutosController@destroy');
-    Route::get('{id}/edit', 'ProdutosController@edit');
-    Route::put('{id}/update', 'ProdutosController@update');
+    Route::get('', ['as' => 'produtos', 'uses' => 'ProdutosController@index']);
+    
+    Route::get('create', ['as' => 'produtos.create', 'uses' => 'ProdutosController@create']);
+    
+    Route::post('store', ['as' => 'produtos.store', 'uses' => 'ProdutosController@store']);
+    
+    Route::get('{id}/destroy', ['as' => 'produtos.destroy', 'uses' => 'ProdutosController@destroy']);
+    
+    Route::get('{id}/edit', ['as' => 'produtos.edit', 'uses' => 'ProdutosController@edit']);
+    
+    Route::put('{id}/update', ['as' => 'produtos.update', 'uses' => 'ProdutosController@update']);
 });
